88974d0b6cd0482d89c3fcadba8c0b501b94efca
Jaya Mukesh <61919375+fishcupboard@users.noreply.github.com>
2021-03-26 16:32:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,11 +83,14 @@ if (__name__ == "__main__"):
    """
 
 from flask import Flask
+
+
+app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "vasanth akka"
 
-app = Flask(__name__)
 
 if (__name__ == "__main__"):
     app.run()
